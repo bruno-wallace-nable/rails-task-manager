33c85dd3343809d80b3ba74d245eec546c64c4db
Bruno Wallace <brunownm@gmail.com>
2026-03-05 16:52:56 -0300

    task manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,16 @@
 class TasksController < ApplicationController
-  # 1. Listar todas as tarefas
   def index
     @tasks = Task.all
   end
 
-  # 2. Ver detalhes de uma tarefa específica
   def show
     @task = Task.find(params[:id])
   end
 
-  # 3. Exibir o formulário de nova tarefa
   def new
     @task = Task.new
   end
 
-  # 4. Salvar a nova tarefa no banco
   def create
     @task = Task.new(task_params)
     if @task.save
@@ -24,12 +20,10 @@ class TasksController < ApplicationController
     end
   end
 
-  # 5. Exibir o formulário de edição
   def edit
     @task = Task.find(params[:id])
   end
 
-  # 6. Salvar as alterações da edição
   def update
     @task = Task.find(params[:id])
     if @task.update(task_params)
@@ -39,7 +33,6 @@ class TasksController < ApplicationController
     end
   end
 
-  # 7. Excluir uma tarefa
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
@@ -48,7 +41,6 @@ class TasksController < ApplicationController
 
   private
 
-  # Método de segurança: define quais campos o usuário pode preencher
   def task_params
     params.require(:task).permit(:title, :details, :completed)
   end
